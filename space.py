a4fd9c02bbe8dc34cf06e8da0a3403fd69a40061
igorzakhar <igorzakhar@gmail.com>
2019-04-06 08:27:51 +0300

    Step 3: Draw blinking star

--- space.py
+++ space.py
@@ -4,10 +4,25 @@ import curses
 
 def draw(canvas):
     canvas.border()
+    curses.curs_set(False)
     row, column = (5, 20)
-    canvas.addstr(row, column, 'Hello, World!')
-    canvas.refresh()
-    time.sleep(1)
+    symbol = '*'
+    while True:
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        canvas.refresh()
+        time.sleep(2)
+
+        canvas.addstr(row, column, symbol)
+        canvas.refresh()
+        time.sleep(0.3)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        canvas.refresh()
+        time.sleep(0.5)
+
+        canvas.addstr(row, column, symbol)
+        canvas.refresh()
+        time.sleep(0.3)
 
 
 if __name__ == '__main__':
