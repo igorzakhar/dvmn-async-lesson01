38d4f0e29f514f97bd75dc4621d7640a8c9c6c4e
igorzakhar <igorzakhar@gmail.com>
2019-04-06 22:11:53 +0300

    Step 10: Add shot animation

--- space.py
+++ space.py
@@ -3,6 +3,8 @@ import curses
 import random
 import time
 
+from fire_animation import fire
+
 
 TIC_TIMEOUT = 0.1
 
@@ -12,35 +14,34 @@ async def blink(canvas, row, column, symbol='*'):
     while True:
         if offset == 0:
             canvas.addstr(row, column, symbol, curses.A_DIM)
-            canvas.refresh()
+
             for tic in range(20):
                 await asyncio.sleep(0)
             offset += 1
 
         if offset == 1:
             canvas.addstr(row, column, symbol)
-            canvas.refresh()
+
             for tic in range(3):
                 await asyncio.sleep(0)
             offset += 1
 
         if offset == 2:
             canvas.addstr(row, column, symbol, curses.A_BOLD)
-            canvas.refresh()
+
             for tic in range(5):
                 await asyncio.sleep(0)
             offset += 1
 
         if offset == 3:
             canvas.addstr(row, column, symbol)
-            canvas.refresh()
+
             for tic in range(3):
                 await asyncio.sleep(0)
             offset = 0
 
 
 def stars_generator(height, width, number=50):
-
     for star in range(number):
         y_pos = random.randint(1, height - 2)
         x_pos = random.randint(1, width - 2)
@@ -58,16 +59,22 @@ def main(canvas):
         for row, column, symbol in stars_generator(height, width)
     ]
 
-    while True:
-        for coro in coroutines:
+    start_row = height - 2
+    start_col = width / 2
+    coro_shot = fire(canvas, start_row, start_col)
+    coroutines.append(coro_shot)
 
+    while True:
+        canvas.refresh()
+        index = 0
+        while index < len(coroutines):
+            coro = coroutines[index]
             try:
                 coro.send(None)
             except StopIteration:
                 coroutines.remove(coro)
+            index += 1
 
-        if len(coroutines) == 0:
-            break
         time.sleep(TIC_TIMEOUT)
 
 
